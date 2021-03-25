77a1ab0b4b75b6a3c0911c562d9e60a3b9c2a93b
Anthony Di Tomasso <anthony.ditomasso@outlook.com>
2021-03-25 20:17:28 +0100

    basic ui

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,8 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function Board() {
-  return <div>Bonjour</div>;
+  const [squares, setSquares] = useState(() => [
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+  ]);
+
+  const [player, setPlayer] = useState('red');
+
+  function setNextPlayer() {
+    setPlayer(player === 'red' ? 'blue' : 'red');
+  }
+
+  function color(id: string): string {
+    switch (id) {
+      case 'red':
+        return 'red';
+      case 'blue':
+        return 'blue';
+      default:
+        return 'white';
+    }
+  }
+
+  function renderSquare(row: number, column: number) {
+    const styles = {
+      backgroundColor: color(squares[row][column]),
+    };
+
+    return (
+      <button
+        key={`${row}${column}`}
+        style={styles}
+        type="button"
+        className="square"
+        onClick={() => selectColumn(column)}
+      >
+        {}
+      </button>
+    );
+  }
+
+  function renderRow(row: Array<string>, rowIndex: number) {
+    return (
+      <div className="board-row" key={`${rowIndex}`}>
+        {row.map((value, index) => {
+          return renderSquare(rowIndex, index);
+        })}
+      </div>
+    );
+  }
+
+  function selectColumn(column: number) {
+    const row = availableRowIn(column);
+    if (row === -1) {
+      return;
+    }
+    const squaresCopy = [...squares];
+    squaresCopy[row][column] = player;
+    setSquares(squaresCopy);
+    setNextPlayer();
+  }
+
+  function availableRowIn(column: number): number {
+    let lastIndex = -1;
+    squares.forEach((value, index) => {
+      if (value[column] === '') {
+        lastIndex = index;
+      }
+    });
+    return lastIndex;
+  }
+
+  return (
+    <div>
+      <h6>player: {player}</h6>
+      {squares.map((value, index) => {
+        return renderRow(value, index);
+      })}
+    </div>
+  );
 }
 
 export default Board;
