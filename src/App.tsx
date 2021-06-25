39167779f52fd3d094e4b0eadb96f72e65934352
Anthony Di Tomasso <anthony.ditomasso@portima.com>
2021-06-25 13:39:21 +0200

    Remove mastermind related classes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
-import MastermindPage from './components/mastermind/MastermindPage';
 import Puissance4Page from './components/fourInARow/Puissance4Page';
 import Puissance4ReducerPage from './components/fourInARow/Puissance4ReducerPage';
 
@@ -15,7 +14,6 @@ function App() {
           <Switch>
             <Route path="/" exact component={Puissance4Page} />
             <Route path="/P4reducer" exact component={Puissance4ReducerPage} />
-            <Route path="/Mastermind" exact component={MastermindPage} />
             <Route path="/" component={() => <h1>You don&apos;t have to be here!</h1>} />
           </Switch>
         </div>
